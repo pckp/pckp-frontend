3df2092b175eac0dd1cceb659aff134b1c6fd284
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-05 03:58:11 -0400

    added a tagbadge

---
+++ src/components/TagBadge.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export interface Tag {
+    tagName: string;
+    count: number
+};
+
+export const tagsList: Tag[]  = [
+    {
+        tagName: "graphics",
+        count: 5
+    },
+    {
+        tagName: "utility",
+        count: 2
+    }
+];
+
+const TagBadge: React.FC<Tag> = (props) => {
+    let {tagName, count} = props;
+
+    return (
+        <button type="button" className="btn btn-primary mt-3">
+            {tagName} <span className="badge bg-secondary">{count}</span>
+        </button>
+    )
+}
+
+export default TagBadge
