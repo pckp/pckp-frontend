74b2c4f1ddac6ace605c396dd98870fae20d146a
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-05 03:57:14 -0400

    changed layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,44 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { Switch, Route, useLocation} from 'react-router-dom'
 import List from "pages/List"
 import Header from 'components/Header'
-
-import "css/index.css"
+import "scss/index.css"
+import Nav from 'components/Nav'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import "scss/bootstrap.scss"
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import Profile from 'pages/Profile'
+import Home from "pages/Home"
 
 const App: React.FC = () => {
-    return <>
-        <Router>
-            <Header />
+
+    return (
+        <>
+            {useLocation().pathname.startsWith("/home") ? null : <Header />}
 
             <Switch>
+                <Route path="/home">
+                    <Home />
+                </Route>
+
                 <Route path="/list">
-                    <List />
+                    <div className="row p-3">
+                        <div className="col-2">
+                            <Nav />
+                        </div>
+
+                        <div className="col-10">
+                            <List />
+                        </div>
+                    </div>
+                </Route>
+
+                <Route path="/profile">
+                    <Profile author="spu7nix" verified={true} />
                 </Route>
             </Switch>
-        </Router>
-    </>
+        </>
+    )
 }
 
 export default App
