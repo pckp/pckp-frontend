a4ca25c04fc6beca336c05073cfa02690e951a39
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-05 03:58:19 -0400

    added home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <div
+            style={{ backgroundColor: '#111119', height: '100vh' }}
+            className="w-100 container-fluid"
+        >
+            <div className="row h-50 d-flex align-items-end justify-content-center">
+                <p
+                    style={{
+                        fontFamily: 'Ubuntu',
+                        fontSize: '8em',
+                        color: 'white',
+                        textAlign: 'center'
+                    }}
+                >
+                    <em>PCKP</em>
+                </p>
+            </div>
+
+            <form className="row h-50 d-flex align-items-start justify-content-center">
+                <input
+                    type="search"
+                    id="search"
+                    className="form-control w-25"
+                    autoComplete="off"
+                    placeholder="Search packages"
+                    style={{
+                        height: '3em',
+                        fontFamily: 'Ubuntu',
+                        fontSize: '1.5em',
+                        display: 'block'
+                    }}
+                />
+                <label className="form-label" htmlFor="search"></label>
+            </form>
+        </div>
+    )
+}
+
+export default Home
