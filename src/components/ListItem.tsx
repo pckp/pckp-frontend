82c5a83c10ebcc7850ec731c4bd84faf4a25a70e
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-06 01:43:59 -0400

    formatting and sass styles

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,32 +1,45 @@
 import React from 'react'
-import styles from "scss/modules/ListItem.module.scss"
+import styles from 'scss/modules/ListItem.module.scss'
 
 interface Props {
-    name: string;
-    description: string;
-    version: string;
-    downloads: number;
-    size: number;
-    added_at: string;
-    versions: number;
-    author: string;
+    name: string
+    description: string
+    version: string
+    downloads: number
+    size: number
+    added_at: string
+    versions: number
+    author: string
 }
 
 const ListItem: React.FC<Props> = (props) => {
-    let {name, description, version, downloads, size, added_at, versions, author} = props;
+    let {
+        name,
+        description,
+        version,
+        downloads,
+        size,
+        added_at,
+        versions,
+        author,
+    } = props
 
     return (
         <div className="ListItem mb-3">
             <div className="card">
                 <div className="card-body">
-                    <h5 className={styles.cardtitle}>
+                    <h5
+                        className={styles.cardtitle}
+                        style={{ display: 'inline-block', wordWrap: "break-word", width: "100%" }}
+                    >
                         {name}
                         <sup>
                             <em>v{version}</em>
                         </sup>
-                        <span style={{ color: 'black' }}>
-                            {' '}
-                            • {author}
+                    </h5>
+                    <h5 style={{ display: 'inline-block', float: 'right' }}>
+                        <span>
+                            {author}
                             <sup>✓</sup>
                         </span>
                     </h5>
@@ -41,7 +54,7 @@ const ListItem: React.FC<Props> = (props) => {
                         <br />
                         Size: {size}kb
                         <br />
-                        <p>Added {added_at}</p>
+                        Added {added_at}
                     </p>
                 </div>
             </div>
