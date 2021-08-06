ccee1b2140b5a756a2df1dde9ae969456459f46c
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-06 01:43:38 -0400

    removed excess jsx into actaul page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Switch, Route, useLocation} from 'react-router-dom'
-import List from "pages/List"
+import Search from "pages/Search"
 import Header from 'components/Header'
 import "scss/index.css"
-import Nav from 'components/Nav'
+import Nav from 'components/TagList'
 import "scss/bootstrap.scss"
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import Profile from 'pages/Profile'
@@ -14,23 +14,17 @@ const App: React.FC = () => {
 
     return (
         <>
-            {useLocation().pathname.startsWith("/home") ? <LoginBar /> : <Header />}
+            {useLocation().pathname.startsWith('/home') ? null : <Header />}
+
+            <LoginBar />
 
             <Switch>
                 <Route path="/home">
                     <Home />
                 </Route>
 
-                <Route path="/list">
-                    <div className="row p-3">
-                        <div className="col-2">
-                            <Nav />
-                        </div>
-
-                        <div className="col-10">
-                            <List />
-                        </div>
-                    </div>
+                <Route path="/search">
+                    <Search />
                 </Route>
 
                 <Route path="/profile">
---
+++ src/pages/Search.tsx
@@ -0,0 +1,58 @@
+import ListItem from 'components/ListItem'
+import TagList from 'components/TagList'
+import useQuery from 'hooks/useQuery'
+import React from 'react'
+import styles from 'scss/modules/Search.module.scss'
+
+const Search: React.FC = () => {
+    const query = useQuery().get('q')
+
+    return (
+        <>
+            <div className="container-fluid">
+                <div className={styles.box}>
+                    <p className={styles.boxText}>
+                        Search Results for:{' '}
+                        <strong>
+                            <em>{query}</em>
+                        </strong>
+                    </p>
+                </div>
+
+                <div className={styles.results}>
+                    <div className={styles.resultsTags}>
+                        <TagList />
+                    </div>
+
+                    <div className={styles.resultsList}>
+                        <ListItem
+                            name="example package"
+                            description="Dolor mollit eiusmod irure culpa dolore incididunt laborum."
+                            version="3.4"
+                            downloads={5}
+                            size={512}
+                            added_at="25/25/25"
+                            versions={3}
+                            author="spu7nix"
+                        />
+
+                        <ListItem
+                            name={"a".repeat(255)}
+                            description="The one and only tensor library for spwn"
+                            version="0.0.8"
+                            downloads={69}
+                            size={42}
+                            added_at="23/23/56"
+                            versions={2}
+                            author="feverdreme"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className="container"></div> */}
+        </>
+    )
+}
+
+export default Search
