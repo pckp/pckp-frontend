9bc6fbdc060273d9f467825d1704120d946f80a2
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-06 18:35:29 -0400

    make bootstrap for input and added form handling

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import pckpimg from 'images/pckp.png'
 import styles from 'scss/modules/TopBar.module.scss'
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 const TopBar: React.FC = () => {
+    let history = useHistory();
+    const [search, setSearch] = useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        history.replace("/search?q=" + search);
+    }
+
     return (
         <>
             <nav className="navbar" style={{ backgroundColor: '#111119' }}>
@@ -11,18 +26,15 @@ const TopBar: React.FC = () => {
                         <img src={pckpimg} alt="pckp" />
                         <em>PCKP</em>
                     </a>
-                    <form className="d-flex">
+                    <form className={styles.navForm} onSubmit={handleSubmit}>
                         <input
                             type="search"
                             id="search"
                             autoComplete="off"
                             placeholder="Search packages"
-                            className="form-control"
-                            style={{
-                                fontSize: '1em',
-                                height: '3em',
-                                width: '25vw',
-                            }}
+                            className={styles.navInput}
+                            onChange={handleChange}
+                            value={search}
                         />
                         <label className="form-label" htmlFor="search"></label>
                     </form>
