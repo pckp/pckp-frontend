716c971396ffdf3d9b63ca7ad22ed82b37309d07
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-07 15:11:15 -0400

    made home page responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,41 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
+import styles from 'scss/modules/Home.module.scss'
 
 const Home: React.FC = () => {
-    let history = useHistory();
-    const [search, setSearch] = useState<string>("");
-    
+    let history = useHistory()
+    const [search, setSearch] = useState<string>('')
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
+        setSearch(e.target.value)
     }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        history.push("/search?q=" + search);
+        history.push('/search?q=' + search)
     }
 
     return (
-        <div
-            style={{ backgroundColor: '#111119', height: '100vh', position: "absolute" }}
-            className="w-100 container-fluid"
-        >
-            <div className="row h-50 d-flex align-items-end justify-content-center">
-                <p
-                    style={{
-                        fontFamily: 'Ubuntu',
-                        fontSize: '8em',
-                        color: 'white',
-                        textAlign: 'center'
-                    }}
-                >
+        <div className={styles.home}>
+            <div className={styles.homeBox}>
+                <p className={styles.homeText}>
                     <em>PCKP</em>
                 </p>
             </div>
 
-            <form className="row h-50 d-flex align-items-start justify-content-center" onSubmit={handleSubmit}>
+            <form className={styles.homeBox} onSubmit={handleSubmit}>
                 <input
                     type="search"
                     id="search"
-                    className="form-control w-25"
+                    className={styles.homeInput}
                     autoComplete="off"
                     placeholder="Search packages"
-                    style={{
-                        height: '3em',
-                        fontFamily: 'Ubuntu',
-                        fontSize: '1.5em',
-                        display: 'block'
-                    }}
                     onChange={handleChange}
                     value={search}
                 />
                 <label className="form-label" htmlFor="search"></label>
 
                 {/* dummy input */}
-                <button type='submit' className="d-none"></button>
+                <button type="submit" className="d-none"></button>
             </form>
         </div>
     )
